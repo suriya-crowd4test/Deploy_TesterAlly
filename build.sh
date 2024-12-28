ea2b321913f1694e2ba142a6e89733b8bba5bcb3
suriya-crowd4test <suriya.prakash@crowd4test.com>
2024-12-28 10:11:58 +0530

    Initial commit for deployment

--- build.sh
+++ build.sh
@@ -1,35 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Stop the script if any command fails
-set -e
-
-# Print the current directory to confirm location
-echo "Current Directory: $(pwd)"
-
-# Install dependencies from requirements.txt
-echo "Installing dependencies..."
 pip install -r requirements.txt
 
-# Run database migrations to apply schema changes
-echo "Applying database migrations..."
+python manage.py collectstatic --no-input
 python manage.py migrate
-
-
-
-# Create a superuser programmatically
-echo "Creating superuser..."
-python <<EOF
-from django.contrib.auth.models import User
-
-username = "admin"
-email = "admin@example.com"
-password = "password"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser '{username}' created successfully.")
-else:
-    print(f"Superuser '{username}' already exists.")
-EOF
-
-echo "Deployment build complete!"
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
