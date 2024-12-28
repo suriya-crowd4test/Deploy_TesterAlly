d61950a39e85aca977dfb7623f4638c495eb5dff
suriya-crowd4test <suriya.prakash@crowd4test.com>
2024-12-28 10:22:04 +0530

    Initial commit for deployment

--- build.sh
+++ build.sh
@@ -4,9 +4,19 @@ set -o errexit
 
 pip install -r requirements.txt
 
-
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+
+# Create a superuser with a username, email, and password
+python <<EOF
+from django.contrib.auth.models import User
+
+username = "admin"
+email = "admin@example.com"
+password = "admin1234"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' with email '{email}' created successfully.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
